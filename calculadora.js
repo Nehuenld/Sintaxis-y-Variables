1417545d590187e49b26fc6c78fdd9062135359a
nehuen <onamonzon.32@gmail.com>
2017-10-29 11:52:24 -0300

    agrege git pages

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+var numero1 = prompt("Ingresa un número");
+var numero2 = prompt("Ingresa otro número");
+
+numero1 = Number(numero1);
+numero2 = Number(numero2);
+
+var resultadoSuma = numero1 + numero2 ;
+var resultadoResta = numero1 - numero2;
+var resultadoMultiplicación = numero1 * numero2;
+var resultadoDivisión = numero1 / numero2;
+
+
+alert("el resultado de la suma es: " + resultadoSuma);
+alert("el resultado de la resta es: " + resultadoResta);
+alert("el resultado de la Multiplicación es: "  + resultadoMultiplicación);
+alert("el resultado de la División es: "  + resultadoDivisión);
